839352cd031aa67d4a9fdfa080cdc8db50cdeff3
Marcelo Gaspar <drmpgaspar@gmail.com>
2020-06-01 04:21:26 +0100

    Create about page and test nuxt routes

---
+++ pages/about.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>About page</h1>
+    <p>This is the best plataform for book lovers</p>
+    <div class="my-2">
+      <v-btn to="/books" small outlined rounded color="primary">
+        Primary
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'About puke.io',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'The best platform for book lovers.'
+        }
+      ]
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/books/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Books</h1>
+    <div class="my-2">
+      <nuxt-link to="/about">
+        <v-btn small outlined rounded color="primary">
+          Primary
+        </v-btn>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'Books List',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'The best platform for book lovers.'
+        }
+      ]
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
